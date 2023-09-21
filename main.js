e948e1f775a27e539197dc0b92196dffaa7837c2
bloodor <33715536+bloodor@users.noreply.github.com>
2023-09-21 20:00:53 +0200

    done lives system + handle wrong/right

--- main.js
+++ main.js
@@ -8,45 +8,78 @@ window.onload = function(){
     ["", "", "", "", "", 1],
     ["", "", "", "", "", 1]];
 
+    var backgroundPerYear = ["black", "blue", "yellow", "", "", ""];
+    var bannerPerYear = ["green", "red", "white", "", "", ""];
+
+    var e_date = document.querySelector('#year');
+    var date = 2020;
     var e_question = document.querySelector('#question');
-    var questionNb = 1;
+    var questionNb = 0;
 
     e_question.textContent = `Question : ${questionList[0][0]}`;
 
     var buttons = document.getElementsByTagName('button');
     for (let index = 0; index < buttons.length; index++) {
         const element = buttons[index];
-        element.textContent = `${questionList[questionNb - 1][index + 1]}`
+        element.textContent = `${questionList[questionNb][index + 1]}`
         
     }
 
 
     e_questionNb = document.querySelector('#questionNb');
-    var questionNb = 1;
-
 
     var e_lifePoints = document.querySelector('#lifePoint');
+    var lifePoints = 3;
+
+    var e_gameScreen = document.querySelector('#gameScreen');
+    var e_UI = document.querySelector('#UI');
 
     for (let index = 0; index < buttons.length; index++) {
-        const element = buttons[index];
-        element.addEventListener("click", userAnswer(e));    
+        buttons[index].addEventListener("click", userAnswer);
     }
     
     function userAnswer(e) {
-        let element = e.target;
-        console.log(e);
-        console.log(element);
+        e.target.id == questionList[questionNb][5] ? changeDisplay() : handleWrong(e);
+    }
+
+    function handleWrong(e) {
+        e.target.style.backgroundColor = 'red';
+        lifePoints--;
+        e_lifePoints.textContent = `Lives left : ${lifePoints}`
+        if (lifePoints === 0) {
+            for (let index = 0; index < buttons.length; index++) {
+                const element = buttons[index];
+                element.style.backgroundColor = "red";
+                element.textContent = "You lost!";
+                //voir pour faire pop un bouton dans le DOM avec du js
+            }
+        }
     }
 
     function changeDisplay() {
-        questionNb++;
-        e_questionNb.textContent = `Question ${questionNb} out of 8`
-        console.log(questionList[questionNb - 1][0]);
-        e_question.textContent = `Question : ${questionList[questionNb - 1][0]}`;
+        if (questionNb < 7)
+            questionNb++;
+        
+
+        for (let index = 0; index < buttons.length; index++) {
+            const element = buttons[index];
+            element.style.backgroundColor = 'cadetblue';
+        }
+        
+        //changer le color pour l'image quand elle seront la
+        e_gameScreen.style.backgroundColor = backgroundPerYear[questionNb - 1];
+        e_UI.style.backgroundColor = bannerPerYear[questionNb];
+        
+        e_questionNb.textContent = `Question ${questionNb + 1} out of 8`
+//        console.log(questionList[questionNb][0]);
+        if (date > 1950)
+            date -= 10;
+        e_date.textContent = `${date}`;
+        e_question.textContent = `Question : ${questionList[questionNb][0]}`;
         
         for (let index = 0; index < buttons.length; index++) {
             const element = buttons[index];
-            element.textContent = `${questionList[questionNb - 1][index + 1]}`
+            element.textContent = `${questionList[questionNb][index + 1]}`
             
         }
         return;
