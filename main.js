46b21ba80f6fdf5bdd877e84e8766154ba97dc56
bloodor <33715536+bloodor@users.noreply.github.com>
2023-09-22 14:50:57 +0200

    fireworks at the end done

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 window.onload = function(){
     var questionList = [["What is a \"DApp\" in blockchain technology?", "Smart contract app", "Decentralized application", " Cryptocurrency mining", "Blockchain video game", 1],
     ["What is the term for a type of machine learning where an algorithm learns from its mistakes by iteratively improving its predictions?", "Supervised learning", "Unsupervised learning", "Reinforcement learning", "Coffee break learning", 2],
-    ["", "", "", "", "", 1],
-    ["", "", "", "", "", 1],
-    ["", "", "", "", "", 1],
-    ["", "", "", "", "", 1],
-    ["", "", "", "", "", 1],
-    ["", "", "", "", "", 1]];
-
+    ["What technology, widely used in web development during the 2000s, allowed web pages to dynamically update content without requiring a full page reload\?", "XML", "HTML5", "AJAX", "ASCII art", 2],
+    ["In which year was the Java programming language first released to the public?", "1985", "1995", "2000", "1977", 1],
+    ["Which integrated development environment (IDE), first released in the 1980s, played a significant role in popularizing software development on the IBM PC platform?", "Visual Studio", "CoffeeScript", "Xcode", "Turbo Pascal", 3],
+    ["Which programming language influenced the development of the C programming language in the 1970s?", "FORTRAN", "COBOL", "ALGOL", "LOLCODE", 2],
+    ["What was the original name of the UNIX operating system when it was first developed in the 1960s?", "Multics", "UnixOS", "C-system", "MainframeOS", 0],
+    ["What technology was prevalent in early computers during the 1950s and served as the primary electronic component for logic and amplification?", "Transistors", "Microprocessors", "Silicon chips", "Vacuum tubes", 3]];
+    
     var e_replay = document.querySelector('#replay');
     var backgroundPerYear = ["burlywood", "black", "blue", "yellow", "", ""];
     var bannerPerYear = ["bisque", "green", "red", "white", "", ""];
@@ -23,14 +23,24 @@ window.onload = function(){
     for (let index = 0; index < buttons.length; index++) {
         const element = buttons[index];
         if (element.id !== 'replay') {
+            buttons[index].addEventListener("click", userAnswer);
             element.textContent = `${questionList[0][index + 1]}`            
         } else {
+            buttons[index].addEventListener("click", replay)
             element.style.display = "none";
-            element.addEventListener("click", replay);
         }
     }
 
+    const container = document.querySelector('#fireworks')
+    container.style.display = "none";
+    var isFireworkOn = false;
+
     function replay() {
+        if (isFireworkOn) {
+            container.style.display = "none";
+        }
+        e_UI.style.display = "flex";
+        e_gameScreen.style.display = "flex";
         lifePoints = 3;
         questionNb = 0;
         date = 2020;
@@ -66,11 +76,6 @@ window.onload = function(){
     var e_gameScreen = document.querySelector('#gameScreen');
     var e_UI = document.querySelector('#UI');
 
-    for (let index = 0; index < buttons.length; index++) {
-        if(buttons[index].id != "replay")
-            buttons[index].addEventListener("click", userAnswer);
-    }
-    
     function userAnswer(e) {
         if (lifePoints > 0)
            e.target.id == questionList[questionNb][5] ? changeDisplay() : handleWrong(e);
@@ -95,6 +100,21 @@ window.onload = function(){
     function changeDisplay() {
         if (questionNb < 7)
             questionNb++;
+        else {
+            e_UI.style.display = "none";
+            e_gameScreen.style.display = "none";
+            container.style.display = "flex";
+            const fireworks = new Fireworks.default(container);
+
+            fireworks.start();
+            isFireworkOn = true;
+
+            for (let index = 0; index < buttons.length; index++) {
+                const element = buttons[index];
+                if (element.id === 'replay')
+                    element.style.display = "block"
+            }
+        }
         
 
         for (let index = 0; index < buttons.length; index++) {
@@ -114,13 +134,10 @@ window.onload = function(){
         
         for (let index = 0; index < buttons.length; index++) {
             const element = buttons[index];
-            element.textContent = `${questionList[questionNb][index + 1]}`
+            if (element.id !== "replay")
+                element.textContent = `${questionList[questionNb][index + 1]}`
             
         }
         return;
     }
-
-    
-
-
-};
+}
